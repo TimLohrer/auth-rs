2ca96ad32eedde0f7ea784c4ab69ee61e68eee62
Tim Lohrer <me@timlohrer.de>
2025-10-08 21:21:09 +0200

    feat: added system info to base api endpoint - <https://github.com/TimLohrer/auth-rs/issues/24>

--- backend/src/routes/base.rs
+++ backend/src/routes/base.rs
@@ -1,13 +1,28 @@
+use std::collections::HashMap;
+use dotenv::var;
 use rocket::{get, serde::json::Json};
 
 use crate::models::http_response::HttpResponse;
 
 #[allow(unused)]
-#[get("/", format = "html")]
-pub async fn base() -> Json<HttpResponse<String>> {
+#[get("/")]
+pub async fn base() -> Json<HttpResponse<HashMap<String, String>>> {
     Json(HttpResponse {
         status: 200,
         message: "Welcome to the auth-rs API!".to_string(),
-        data: None,
+        data: Some(HashMap::from_iter(vec![
+            (
+                "version".to_string(),
+                var("VERSION").expect("Missing VERSION in .env!"),
+            ),
+            (
+                "repository".to_string(),
+                "https://github.com/TimLohrer/auth-rs".to_string(),
+            ),
+            (
+                "issues".to_string(),
+                "https://github.com/TimLohrer/auth-rs/issues".to_string(),
+            ),
+        ]))
     })
 }
