35089468e571b7646929026d40f67323a4bdf86f
Tim Lohrer <me@timlohrer.de>
2025-10-08 16:04:12 +0200

    feat: update frontend models to new backend settings & user data storage - <https://github.com/TimLohrer/auth-rs/issues/16>

--- src/lib/models/Settings.ts
+++ src/lib/models/Settings.ts
@@ -1,11 +1,21 @@
 export default class Settings {
-    _id: string;
-    openRegistration: boolean;
-    allowOauthAppsForUsers: boolean;
+	_id: string;
+	version: string;
+	versionHistory: [][];
+	openRegistration: boolean;
+	allowOauthAppsForUsers: boolean;
 
-    constructor(_id: string, openRegistration: boolean, allowOauthAppsForUsers: boolean) {
-        this._id = _id;
-        this.openRegistration = openRegistration;
-        this.allowOauthAppsForUsers = allowOauthAppsForUsers;
-    }
+	constructor(
+		_id: string,
+		version: string,
+		versionHistory: [][],
+		openRegistration: boolean,
+		allowOauthAppsForUsers: boolean
+	) {
+		this._id = _id;
+		this.version = version;
+		this.versionHistory = versionHistory;
+		this.openRegistration = openRegistration;
+		this.allowOauthAppsForUsers = allowOauthAppsForUsers;
+	}
 }
--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -1,38 +1,50 @@
 export default class User {
-    _id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    roles: string[];
-    mfa: boolean;
-    disabled: boolean;
-    createdAt: any;
+	_id: string;
+	email: string;
+	firstName: string;
+	lastName: string;
+	roles: string[];
+	mfa: boolean;
+	dataStorage: Record<string, any> | null;
+	disabled: boolean;
+	createdAt: any;
 
-    constructor(_id: string, email: string, firstName: string, lastName: string, roles: string[], mfa: boolean, disabled: boolean, createdAt: any) {
-        this._id = _id;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.roles = roles;
-        this.mfa = mfa;
-        this.disabled = disabled;
-        this.createdAt = createdAt;
-    }
+	constructor(
+		_id: string,
+		email: string,
+		firstName: string,
+		lastName: string,
+		roles: string[],
+		mfa: boolean,
+		dataStorage: Record<string, any> | null,
+		disabled: boolean,
+		createdAt: any
+	) {
+		this._id = _id;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.roles = roles;
+		this.mfa = mfa;
+		this.dataStorage = dataStorage;
+		this.disabled = disabled;
+		this.createdAt = createdAt;
+	}
 
-    static getCreatedAt(user: User): Date {
-        // @ts-ignore
-        return new Date(parseInt(user.createdAt.$date.$numberLong) ?? 0);
-    }
+	static getCreatedAt(user: User): Date {
+		// @ts-ignore
+		return new Date(parseInt(user.createdAt.$date.$numberLong) ?? 0);
+	}
 
-    static isAdmin(user: User): boolean {
-        return user._id == this.DEFAULT_USER_ID || user.roles.includes(this.ADMIN_ROLE_ID);
-    }
+	static isAdmin(user: User): boolean {
+		return user._id == this.DEFAULT_USER_ID || user.roles.includes(this.ADMIN_ROLE_ID);
+	}
 
-    static isSystemAdmin(user: User): boolean {
-        return user._id == this.DEFAULT_USER_ID;
-    }
+	static isSystemAdmin(user: User): boolean {
+		return user._id == this.DEFAULT_USER_ID;
+	}
 
-    static DEFAULT_USER_ID = '00000000-0000-0000-0000-000000000000';
-    static DEFAULT_ROLE_ID = '00000000-0000-0000-0000-000000000001';
-    static ADMIN_ROLE_ID = '00000000-0000-0000-0000-000000000000';
+	static DEFAULT_USER_ID = '00000000-0000-0000-0000-000000000000';
+	static DEFAULT_ROLE_ID = '00000000-0000-0000-0000-000000000001';
+	static ADMIN_ROLE_ID = '00000000-0000-0000-0000-000000000000';
 }
