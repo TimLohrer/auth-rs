84bf83ebfef0543f75d43e294e2c6ba2e203fbaa
Tim Lohrer <me@timlohrer.de>
2025-10-09 14:13:35 +0200

    fix: show toasts in correct order - <https://github.com/TimLohrer/auth-rs/issues/21>

--- src/lib/components/global/Toasts.svelte
+++ src/lib/components/global/Toasts.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="absolute w-screen h-screen top-0 left-0 z-1000" style="pointer-events: none;">
-    {#each $activeToasts as toast, index}
+    {#each $activeToasts.toReversed() as toast, index}
         <div
             class="flex w-fit min-w-[250px] min-h-[50px] h-[50px] bg-black border-[2.5px] absolute right-5 bottom-5 {toast.type == 'error' ? 'border-red-500' : toast.type == 'success' ? 'border-green-500' : 'border-blue-500'} rounded-md items-center"
             style="margin-bottom: {index * 60}px;"
