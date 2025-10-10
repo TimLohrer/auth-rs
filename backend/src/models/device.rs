5acb5030cb086fe7563b79f02a56bf89fa631321
Tim Lohrer <me@timlohrer.de>
2025-10-10 23:52:14 +0200

    feat: add device id into token claims - <https://github.com/TimLohrer/auth-rs/issues/20>

--- backend/src/models/device.rs
+++ backend/src/models/device.rs
@@ -1,5 +1,6 @@
 use mongodb::bson::{DateTime, Uuid};
 use rocket::serde::{Deserialize, Serialize};
+use serde_json::json;
 use crate::auth::oidc::create_id_token;
 
 #[derive(Debug, Clone, Serialize, Deserialize )]
@@ -27,9 +28,10 @@ pub struct DeviceDTO {
 
 impl Device {
     pub fn new(user_id: Uuid, os: Option<String>, user_agent: String, ip_adress: Option<String>) -> Self {
+        let device_id = Uuid::new();
         Device {
-            id: Uuid::new(),
-            token: create_id_token(user_id.clone(), user_id, None).unwrap_or_default(),
+            id: device_id.clone(),
+            token: create_id_token(user_id.clone(), user_id, Some(json!({ "deviceId": device_id.to_string() }))).unwrap_or_default(),
             os: os.unwrap_or_else(|| "Unknown".to_string()),
             user_agent: user_agent.to_string(),
             ip_address: ip_adress.unwrap_or_else(|| "Unknown".to_string()),
